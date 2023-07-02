f9ed3b042a07c1d43265e1a83822c0a9ea6ab488
buraksenyurt <burakselimsenyurt@gmail.com>
2023-07-02 16:46:32 +0300

    Integrated Sqlite support to the application.

--- EmployeeService/EmployeeService/Program.cs
+++ EmployeeService/EmployeeService/Program.cs
@@ -1,17 +1,24 @@
+using EmployeeService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<EmployeeDbContext>(
+    options => options.UseSqlite("Data Source = EmployeeService.db")
+);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<EmployeeDbContext>();
+    dbContext.Database.EnsureCreated();
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
