f9b9c72c66f781bb6af4292cec799a1f1df401e5
buraksenyurt <burakselimsenyurt@gmail.com>
2023-07-03 23:45:42 +0300

    Fixed the queue consuming problem.

--- Employee/EmployeeService/Queue/IQueue.cs
+++ Employee/EmployeeService/Queue/IQueue.cs
@@ -1,7 +1,8 @@
+using RabbitMQ.Client;
+
 namespace EmployeeService.Queue;
 
 public interface IQueue
 {
-    Task PublishMessage(string key, string data);
-    Task ReadMessage(string key, CancellationToken cancellationToken);
+    Task ReadMessage(IModel channel, string key, CancellationToken cancellationToken);
 }
--- Employee/EmployeeService/Queue/RabbitQueue.cs
+++ Employee/EmployeeService/Queue/RabbitQueue.cs
@@ -11,56 +11,43 @@ public class RabbitQueue
     : IQueue
 {
     private readonly IServiceScopeFactory _serviceFactory;
-    public RabbitQueue(IServiceScopeFactory serviceFactory)
+    private readonly ILogger _logger;
+    public RabbitQueue(IServiceScopeFactory serviceFactory, ILogger<RabbitQueue> logger)
     {
         _serviceFactory = serviceFactory;
+        _logger = logger;
     }
-    private IConnection GetConnection()
+    public async Task ReadMessage(IModel channel, string key, CancellationToken cancellationToken)
     {
-        var factory = new ConnectionFactory
-        {
-            HostName = "127.0.0.1",
-            UserName = "scoth",
-            Password = "tiger"
-        };
-
-        var connection = factory.CreateConnection();
-        return connection;
-    }
-    public async Task PublishMessage(string key, string data)
-    {
-        using var channel = GetConnection().CreateModel();
-
-        var body = Encoding.UTF8.GetBytes(data);
-        channel.BasicPublish(
-            exchange: "sales.exchange",
-            routingKey: key,
-            basicProperties: null,
-            body: body);
-
-        await Task.CompletedTask;
-    }
-
-    public async Task ReadMessage(string key, CancellationToken cancellationToken)
-    {
-        using var channel = GetConnection().CreateModel();
         var consumer = new AsyncEventingBasicConsumer(channel);
         consumer.Received += async (model, args) =>
         {
             var body = Encoding.UTF8.GetString(args.Body.ToArray());
+            _logger.LogWarning($"Gelen veri {body}");
             using var scope = _serviceFactory.CreateScope();
             var dbContext = scope.ServiceProvider.GetRequiredService<EmployeeDbContext>();
             var data = JsonSerializer.Deserialize<Contract>(body);
             var type = args.RoutingKey;
+            _logger.LogWarning($"Routing Key {type}");
             if (type == "insurance.contract")
             {
-                var contract = await dbContext.Contracts.FirstOrDefaultAsync(c => c.ContractId == data.ContractId, cancellationToken);
+                _logger.LogInformation($"Contract ID = {data.ContractId}");
+                var contract = dbContext.Contracts.FirstOrDefault(c => c.ContractId == data.ContractId);
+                _logger.LogInformation($"contract is null= {contract == null}");
                 if (contract == null)
                 {
-                    await dbContext.Contracts.AddAsync(contract, cancellationToken);
+                    _logger.LogInformation($"{data.ContractId} sistemde yok. Yeni eklenecek.");
+                    var newContract = new Contract
+                    {
+                        ContractId = data.ContractId,
+                        Title = data.Title,
+                        Quantity = data.Quantity
+                    };
+                    await dbContext.Contracts.AddAsync(newContract, cancellationToken);
                 }
                 else
                 {
+                    _logger.LogInformation($"{data.ContractId} sistemde var. Bilgileri güncellenecek.");
                     contract.Title = data.Title;
                     contract.Quantity = data.Quantity;
                 }
@@ -69,7 +56,7 @@ public class RabbitQueue
             }
 
         };
-        channel.BasicConsume("insurance.contact", true, consumer);
+        channel.BasicConsume("insurance.contract", true, consumer);
         await Task.CompletedTask;
     }
 }
