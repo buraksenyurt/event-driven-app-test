2512cf50623bd879b0521688717151b51b32fe4d
buraksenyurt <burakselimsenyurt@gmail.com>
2023-07-05 22:37:21 +0300

    Added mvc project for Insurance Service.

---
+++ Employee/Insurance.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Insurance.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
