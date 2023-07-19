7cdae5f2242273e15d3aa63b44f4cf8a2497fdf1
buraksenyurt <burakselimsenyurt@gmail.com>
2023-07-19 12:40:28 +0300

    Changed to theory.

--- Employee/Employee.Test.UI/InsuranceWebTests.cs
+++ Employee/Employee.Test.UI/InsuranceWebTests.cs
@@ -5,8 +5,33 @@ namespace Employee.Test.UI;
 
 public class InsuranceWebTests
 {
-    [Fact]
-    public async Task Should_New_Policy_Added_Works_Test()
+    // [Fact]
+    // public async Task Should_New_Contract_Added_Works_Test()
+    // {
+    //     // Act
+    //     using var pw = await Playwright.CreateAsync();
+    //     await using var browser = await pw.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
+    //     {
+    //         Headless = false
+    //     });
+    //     var page = await browser.NewPageAsync();
+    //     await page.GotoAsync("http://localhost:5131/");
+    //     await page.ClickAsync("text=Poliçe Oluştur");
+    //     await page.FillAsync("input[id=Title]", "Kafama saksı düştü sigortası");
+    //     await page.FillAsync("input[id=Quantity]", "13");
+    //     await page.ClickAsync("input[type=submit]");
+
+    //     // Assertion
+    //     await page.ClickAsync("text=Poliçeler");
+    //     var result = await page.TextContentAsync(".table");
+    //     result.Should().Contain("Kafama saksı düştü sigortası");
+    // }
+
+    [Theory]
+    [InlineData("Telefonumun camı çatladı sigortası", "58")]
+    [InlineData("Cüzdanımı kaybettim sigortası", "17")]
+    [InlineData("Fazla oyun oynamaktan ekran kartım yandı sigortası", "4")]
+    public async Task Should_Contracts_Data_Set_Added_Works_Test(string title, string quantity)
     {
         // Act
         using var pw = await Playwright.CreateAsync();
@@ -17,13 +42,13 @@ public class InsuranceWebTests
         var page = await browser.NewPageAsync();
         await page.GotoAsync("http://localhost:5131/");
         await page.ClickAsync("text=Poliçe Oluştur");
-        await page.FillAsync("input[id=Title]", "Kafama saksı düştü sigortası");
-        await page.FillAsync("input[id=Quantity]", "13");
+        await page.FillAsync("input[id=Title]", title);
+        await page.FillAsync("input[id=Quantity]", quantity);
         await page.ClickAsync("input[type=submit]");
 
         // Assertion
         await page.ClickAsync("text=Poliçeler");
         var result = await page.TextContentAsync(".table");
-        result.Should().Contain("Kafama saksı düştü sigortası");
+        result.Should().Contain(title);
     }
 }
