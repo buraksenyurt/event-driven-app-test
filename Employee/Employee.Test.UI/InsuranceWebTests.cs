dec78cddcea9da6be2289f099857fbac6291e2e0
buraksenyurt <burakselimsenyurt@gmail.com>
2023-07-25 21:10:08 +0300

    Applied autofixture

--- Employee/Employee.Test.UI/InsuranceWebTests.cs
+++ Employee/Employee.Test.UI/InsuranceWebTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.Playwright;
 using FluentAssertions;
+using AutoFixture;
 
 namespace Employee.Test.UI;
 
@@ -27,23 +28,51 @@ public class InsuranceWebTests
     //     result.Should().Contain("Kafama saksı düştü sigortası");
     // }
 
-    [Theory]
-    [InlineData("Telefonumun camı çatladı sigortası", "58")]
-    [InlineData("Cüzdanımı kaybettim sigortası", "17")]
-    [InlineData("Fazla oyun oynamaktan ekran kartım yandı sigortası", "4")]
-    public async Task Should_Contracts_Data_Set_Added_Works_Test(string title, string quantity)
+    // [Theory]
+    // [InlineData("Telefonumun camı çatladı sigortası", "58")]
+    // [InlineData("Cüzdanımı kaybettim sigortası", "17")]
+    // [InlineData("Fazla oyun oynamaktan ekran kartım yandı sigortası", "4")]
+    // public async Task Should_Contracts_Data_Set_Added_Works_Test(string title, string quantity)
+    // {
+    //     // Act
+    //     using var pw = await Playwright.CreateAsync();
+    //     await using var browser = await pw.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
+    //     {
+    //         Headless = false
+    //     });
+    //     var page = await browser.NewPageAsync();
+    //     await page.GotoAsync("http://localhost:5131/");
+    //     await page.ClickAsync("text=Poliçe Oluştur");
+    //     await page.FillAsync("input[id=Title]", title);
+    //     await page.FillAsync("input[id=Quantity]", quantity);
+    //     await page.ClickAsync("input[type=submit]");
+
+    //     // Assertion
+    //     await page.ClickAsync("text=Poliçeler");
+    //     var result = await page.TextContentAsync(".table");
+    //     result.Should().Contain(title);
+    // }
+
+    [Fact]
+    public async Task Should_Contracts_Data_Set_Added_Works_Test()
     {
-        // Act
+        // Arange
         using var pw = await Playwright.CreateAsync();
         await using var browser = await pw.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
         {
             Headless = false
         });
+
+        var title = new Fixture().Create<string>();
+        var quantity = new Fixture().Create<int>();
+
         var page = await browser.NewPageAsync();
+
+        // Act
         await page.GotoAsync("http://localhost:5131/");
         await page.ClickAsync("text=Poliçe Oluştur");
         await page.FillAsync("input[id=Title]", title);
-        await page.FillAsync("input[id=Quantity]", quantity);
+        await page.FillAsync("input[id=Quantity]", quantity.ToString());
         await page.ClickAsync("input[type=submit]");
 
         // Assertion
