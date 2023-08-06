be4d5631fce3345ce6fabd81e6b64abcbc1628ee
buraksenyurt <burakselimsenyurt@gmail.com>
2023-08-06 20:42:48 +0300

    Changed to program class for integration tests.

--- Employee/EmployeeService/Program.cs
+++ Employee/EmployeeService/Program.cs
@@ -4,38 +4,46 @@ using EmployeeService.Data;
 using EmployeeService.Queue;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
+namespace EmployeeService;
 
-builder.Services.AddDbContext<EmployeeDbContext>(
-    options => options.UseSqlite("Data Source = EmployeeService.db")
-);
-builder.Services
-    .AddRabbitMqSettings(configuration)
-    .AddSingleton<IQueueHandler, RabbitMqHandler>()
-    .AddScoped<IRabbitMqScopedService, RabbitMqScopedService>()
-    .AddHostedService<RabbitMqService>();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    using var scope = app.Services.CreateScope();
-    var dbContext = scope.ServiceProvider.GetRequiredService<EmployeeDbContext>();
-    dbContext.Database.EnsureCreated();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var configuration = builder.Configuration;
 
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        builder.Services.AddDbContext<EmployeeDbContext>(
+            options => options.UseSqlite("Data Source = EmployeeService.db")
+        );
+        builder.Services
+            .AddRabbitMqSettings(configuration)
+            .AddSingleton<IQueueHandler, RabbitMqHandler>()
+            .AddScoped<IRabbitMqScopedService, RabbitMqScopedService>()
+            .AddHostedService<RabbitMqService>();
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            using var scope = app.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<EmployeeDbContext>();
+            dbContext.Database.EnsureCreated();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
--- Employee/InsuranceService/Program.cs
+++ Employee/InsuranceService/Program.cs
@@ -4,38 +4,46 @@ using InsuranceService.Data;
 using InsuranceService.Queue;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
+namespace InsuranceService;
 
-builder.Services.AddDbContext<ContractDbContext>(options =>
-    options.UseSqlite("Data Source=InsuranceService.db"));
-
-builder.Services
-    .AddRabbitMqSettings(configuration)
-    .AddSingleton<IQueueHandler, RabbitMqHandler>()
-    .AddScoped<IRabbitMqScopedService, RabbitMqScopedService>()
-    .AddHostedService<RabbitMqService>();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    using var scope = app.Services.CreateScope();
-    var dbContext = scope.ServiceProvider.GetRequiredService<ContractDbContext>();
-    dbContext.Database.EnsureCreated();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var configuration = builder.Configuration;
 
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        builder.Services.AddDbContext<ContractDbContext>(options =>
+            options.UseSqlite("Data Source=InsuranceService.db"));
+
+        builder.Services
+            .AddRabbitMqSettings(configuration)
+            .AddSingleton<IQueueHandler, RabbitMqHandler>()
+            .AddScoped<IRabbitMqScopedService, RabbitMqScopedService>()
+            .AddHostedService<RabbitMqService>();
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            using var scope = app.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ContractDbContext>();
+            dbContext.Database.EnsureCreated();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
